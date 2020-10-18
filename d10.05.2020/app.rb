b69a2ace0047edd8d365489512e7a46f740aafb0
AnnaAleynik <anna.mikhailovna@gmail.com>
2020-10-18 22:52:08 +0300

    class ToDo || added

--- d10.05.2020/app.rb
+++ d10.05.2020/app.rb
@@ -2,61 +2,75 @@ require 'pg'
 require 'sinatra'
 require 'byebug'
 require 'sinatra/reloader' if development?
+require './todo.rb'
+
 
 configure {set :server, :puma}
 
-conn = PG.connect( dbname: 'todo_app', host: 'localhost', user: 'postgres',
-    password: 'postgres' )
 
+
+# main page
 get '/' do
 	'Hello'
 end
 
-get "/todos" do
-
-	@results = conn.exec( "SELECT * FROM todos")
-
+# show all todos
+get "/todos" do	
+	@results = ToDo.all
 	erb :index
 
 end 
 
+# show input new todo
 get "/todos/new" do
-	erb :new
+  erb :new
 end
 
-get "/todos/:id/edit" do
-	@results = conn.exec("SELECT * FROM todos WHERE id='#{params['id']}'")[0]
 
+
+
+# show edit-input todo
+get "/todos/:id/edit" do
+	# @results = conn.exec("SELECT * FROM todos WHERE id='#{params['id']}'")[0]
+
+	@results = ToDo.where({"id" => params['id']})[0]
   erb :edit
 end
+
+
 
 get '/todos/:id' do
   "Hello World #{params["id"]}"
 
-  @results = conn.exec("SELECT * FROM todos WHERE id='#{params['id']}'")[0]
+  # @results = conn.exec("SELECT * FROM todos WHERE id='#{params['id']}'")[0]
 
+	@results = ToDo.where({"id"=>params['id']})[0]
   erb :show
 end
 
 
+
 post '/todos' do
 	if params["custom_method"] == "DELETE"
-		res = conn.exec("DELETE FROM todos WHERE id=#{params['id']}")
+		
+		todo = ToDo.new(params['id'], params['title'])
+		todo.delete
 
-		redirect to('/todos')
+    redirect to('/todos')
 	else 
-		res = conn.exec("INSERT INTO todos (title) VALUES ('#{params['title']}') RETURNING id")
-
-		id = res[0]["id"]
+		todo = ToDo.new(params['id'], params['title'])
+		id = todo.save
 		redirect to("/todos/#{id}")
 	end
 end
 
+
 post "/todos/:id" do
 	if params["custom_method"] == "PUT"
-		res = conn.exec("UPDATE todos SET title='#{params['title']}'' WHERE id=#{params['id']} RETURNING id")
-		id = params["id"]
 
+		todo = ToDo.new(params['id'], params['title'])
+		todo.update
+		
 		redirect to("/todos/#{id}")
 	end
 end
---
+++ d10.05.2020/todo.rb
@@ -0,0 +1,90 @@
+require 'pg'
+require 'sinatra'
+
+
+class ToDo
+
+  attr_reader :id, :title
+  @@conn = PG.connect( dbname: 'todo_app', host: 'localhost', user: 'postgres',
+    password: 'postgres' )
+
+
+  def initialize(id, title)
+        # @results = conn.exec( "SELECT * FROM todos")
+    @id = id
+    @title = title
+  end
+
+  # def initialize(hash) 
+  #   hash.each_pair do |k, v|
+
+  #   end
+  # end
+
+  def self.mapping(hash)
+    hash.map do |entry|
+      entry = ToDo.new(entry['id'], entry['title'])
+      # entry = ToDo.new(entry)
+
+    end
+
+  end
+
+  def self.all
+    results = @@conn.exec("SELECT * FROM todos")
+    self.mapping(results)
+  end
+
+  def self.where(params)
+    id_str = ''
+    title_str = ''
+    method_str = ''
+
+    if (params["meth"] == nil) 
+      method_str = "SELECT * FROM todos WHERE "
+    elsif (params["meth"] == 'DELETE')
+      method_str = "DELETE FROM todos WHERE "
+    end
+      
+
+    if (params["id"] != nil)
+      id_str = "id='#{params["id"]}'"
+    end
+
+    if (params["title"] != nil)
+      id_str += " and " if id_str != ''
+      title_str = "title='#{params["title"]}'"
+    end
+        
+    res = @@conn.exec("#{method_str}#{id_str} #{title_str};")
+    self.mapping(res)
+    # res = ToDo.new(res['id'], res['title'])
+  # @results = conn.exec("SELECT * FROM todos WHERE id='#{params['id']}'")[0]
+
+  end
+
+
+  def save
+    res = @@conn.exec("INSERT INTO todos (title) VALUES ('#{@title}') RETURNING id")
+    puts res.class.name
+
+    id = res[0]
+    
+  end
+
+
+  def delete
+    res = @@conn.exec("DELETE FROM todos WHERE id=#{@id}")
+  end
+
+  def update
+    res = conn.exec("UPDATE todos SET title='#{@title}' WHERE id=#{@id} RETURNING id")
+    id = @id
+    # id = res
+
+
+    
+  end
+
+
+end
